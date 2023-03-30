a733e4bc0e963fa26b3c20e583c4e993be1cd1a0
EmmanuelBahena <mannybahena04@gmail.com>
2023-03-30 13:23:35 -0500

    hardcoded products create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,7 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params[:id])
     render :show
   end  
-end 
+  def create
+    @product = Product.new(name: "fire_truck", price: "4", image_url: "https://www.3000toys.com/images/ERTL/46731-CNP.jpg", description: "its loud and red")
+  end
+end  
