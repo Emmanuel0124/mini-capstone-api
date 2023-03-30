07fd26064e5e4b579b8f37df6a10d3909c7e617a
EmmanuelBahena <mannybahena04@gmail.com>
2023-03-30 12:24:18 -0500

    products show test

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -8,4 +8,12 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Product.count, data.length
   end
+
+  test "show" do
+    get "/products/#{Product.first.id}.json"
+    assert_response 200
+  
+    data = JSON.parse(response.body)
+    assert_equal ["id", "name", "price", "image_url", "description", "created_at", "updated_at"], data.keys
+  end
 end
