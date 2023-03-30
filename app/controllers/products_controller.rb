9a3b6e8972712280d6d047178f7782bc79edb027
EmmanuelBahena <mannybahena04@gmail.com>
2023-03-30 11:59:15 -0500

    clear out non-restful routs

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,3 @@
 class ProductsController < ApplicationController
-  def singular
-    @product = Product.find_by(id: params["id"])
-    render template: "products/show"
-  end 
-
-  def index 
-    @products = Product.all 
-    render template: "products/index"
-  end
+  
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  get "/one_product/:id" => "products#singular"
-  get "/products" => "products#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
