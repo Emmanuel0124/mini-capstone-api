4c5a84b27ab187fc1604cac22db92de6aab6e4fc
EmmanuelBahena <mannybahena04@gmail.com>
2023-03-30 15:20:37 -0500

    product update hardcoded

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,4 +17,13 @@ class ProductsController < ApplicationController
     @product.save
     render :show
   end
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = "action_figure"
+    @product.price = 8
+    @product.image_url = "https://ae01.alicdn.com/kf/Sc4521cc7aa7e4d39a058d63ca6693c1fE/Japan-Anime-Dragon-Ball-Z-Original-BANDAI-SPIRITS-S-H-Figuarts-SHF-Exclusive-Action-Figure-Great.jpg"
+    @product.description = "posable super hero"
+    @product.save
+    render :show
+  end 
 end  
