e6a690a53eb61582ce89cd800869ced75bce93ed
EmmanuelBahena <mannybahena04@gmail.com>
2023-04-17 13:09:28 -0500

    add sessions create action

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   post "/products" => "products#create"
   patch "/products/:id" => "products#update"
   post "/users" => "users#create"
+  post "/sessions" => "sessions#create"
 end
