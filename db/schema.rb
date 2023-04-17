36da079347917a352215c52472abf97a2c3cba7c
EmmanuelBahena <mannybahena04@gmail.com>
2023-04-17 11:37:06 -0500

    add supplier model and attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_29_184713) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_10_174406) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_184713) do
     t.integer "price"
     t.string "image_url"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
