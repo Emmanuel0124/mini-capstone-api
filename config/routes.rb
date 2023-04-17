0ec3c691162f5dbcd0484f08b59dc98b769dad6d
EmmanuelBahena <mannybahena04@gmail.com>
2023-04-17 12:36:55 -0500

    add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "/products/:id" => "products#show"
   post "/products" => "products#create"
   patch "/products/:id" => "products#update"
+  post "/users" => "users#create"
 end
